54c540b2525821e3a7f76df40c639991934983a9
BeauConfra <beau@confra.xyz>
2025-01-20 11:57:43 +1100

    Update page.js

--- src/app/client/page.js
+++ src/app/client/page.js
@@ -17,14 +17,17 @@ export default function ClientsPage() {
       <FadeInSection>
         <main className="flex-grow flex flex-col items-center justify-center text-center px-4 py-24">
           <h1 className="text-[#cdff00] font-bold text-4xl lg:text-6xl leading-tight mb-4">
-            Your Web3 Conference Booth Experts
+            Maximize Lead Gen at Web3 Conferences
           </h1>
-          <p className="text-white/70 text-lg md:text-xl max-w-4xl py-5">
-            Sponsoring a booth at Web3 events is a great way to generate leads.  Staffing the booth, on the other hand, is a pain.  You pull expensive, important resources away from your business, or rely on contractors who don’t know your business or even Web3. 
-          </p>
-          <p className="text-white/70 text-lg md:text-xl max-w-4xl mb-10">
-            Confra Booth BD solves this - we provide Web3-native experts, sourced from the local community and trained on your product.  These BD act as an extension of your team, finding and qualifying the right leads, allowing you to deliver more leads from more conferences.
-          </p>
+          <p className="text-white/70 text-lg md:text-xl max-w-4xl mb-8 py-5">
+          Confra is your dedicated Web3 conference business development team. We 
+          supercharge your sales pipeline by deploying skilled, technical Web3 BD 
+          to top Web3 conferences, delivering high-quality, qualified leads 
+          straight to you. Confra BD have deep experience in L1/L2s, infrastructure, 
+          DeFi, RWAs, Stables, Memecoins, Payments, Custody, Tokenomics, 
+          Market-Making, and more...
+        </p>
+
           <div className="flex flex-col sm:flex-row gap-x-10 gap-y-5 justify-center mb-16">
             <Button onClick={handleOpenModal}>Get Started</Button>
           </div>
@@ -38,7 +41,7 @@ export default function ClientsPage() {
           How It Works
         </h2>
         <p className="text-gray-900 text-base sm:text-lg font-normal leading-7 mb-20">
-          From preparation to lead-delivery, we take care of it all.
+          From preparation to lead-delivery, we take care of it all
         </p>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
@@ -49,7 +52,7 @@ export default function ClientsPage() {
               Pre-Conference
             </h3>
             <p className="text-gray-700 text-base font-normal leading-normal max-w-xs">
-              You approve dedicated Confra Booth BD, who then get trained on your product, value prop and lead qualification criteria.  We then create the detailed booth coverage schedules to ensure maximum coverage at all times.
+            You approve your Confra BD, who will learn your business, align on targeting and plan every conference day in order to maximise leads.
             </p>
           </div>
           
@@ -60,8 +63,7 @@ export default function ClientsPage() {
               On-Site Coverage
             </h3>
             <p className="text-gray-700 text-base font-normal leading-normal max-w-xs">
-              Skilled BD professionals engage attendees, qualify leads, field questions, display product features and drive conversations.  We can use the Confra lead capture tools or your own.
-            </p>
+              Expert BD reps engage with prospects at pre-arranged meetings, booths, side events, and networking sessions.            </p>
           </div>
           
           {/* Post-Conference */}
@@ -71,7 +73,7 @@ export default function ClientsPage() {
               Post-Conference
             </h3>
             <p className="text-gray-700 text-base font-normal leading-normal max-w-xs">
-              Get summary reports and qualified leads lists for your team to follow up.
+              Receive prioritized, CRM-ready leads and conference summaries.
             </p>
           </div>
         </div>
@@ -85,7 +87,7 @@ export default function ClientsPage() {
           Benefits
         </h2>
         <p className="text-white/70 text-base sm:text-lg font-normal leading-7 mb-20">
-          Quality leads, industry expertise, and end-to-end support.
+          Quality Leads, Industry Expertise, and End-to-End Support
         </p>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
@@ -96,7 +98,7 @@ export default function ClientsPage() {
               Quality Leads
             </h3>
             <p className="text-white/70 text-base font-normal leading-normal max-w-xs">
-              Confra Booth BD are both Web3-natives and sales professionals, ensuring the post-conference leads lists are expertly qualified.
+              Pay only for leads that result in meaningful follow-ups.
             </p>
           </div>
 
@@ -104,10 +106,10 @@ export default function ClientsPage() {
           <div className="flex flex-col items-center">
             <img src='industry.png' className="w-9 h-9 mb-6" />
             <h3 className="text-white text-2xl sm:text-3xl font-bold leading-10 mb-2">
-              Industry and Regional Expertise
+              Industry Expertise
             </h3>
             <p className="text-white/70 text-base font-normal leading-normal max-w-xs">
-              Confra Booth BD excel in DeFi, infrastructure, custody, markets, security and more, and are fluent in the language of the region the event is hosted in.
+              Our BDs specialize in L1/L2, DeFi, infrastructure, stablecoins, and more.
             </p>
           </div>
 
@@ -118,7 +120,7 @@ export default function ClientsPage() {
               Customized Approach
             </h3>
             <p className="text-white/70 text-base font-normal leading-normal max-w-xs">
-              Align messaging with objectives, ensuring brand synergy that resonates with attendees of the conference.
+              Fully align brand representation and messaging with your goals.
             </p>
           </div>
         </div>
@@ -137,7 +139,7 @@ export default function ClientsPage() {
           <div className="flex flex-col items-center pb-10 md:pb-0">
             <img src='dollar.png' className="w-6 h-9 mb-6" />
             <p className="text-gray-700 text-base font-normal leading-normal max-w-xs">
-              Fixed daily fee, covering training and BD’s time at the conference.
+              Flat daily fee, covering BD’s time, per diem and travel costs
             </p>
           </div>
 
@@ -145,7 +147,7 @@ export default function ClientsPage() {
           <div className="flex flex-col items-center">
             <img src='graph.png' className="w-9 h-9 mb-6" />
             <p className="text-gray-700 text-base font-normal leading-normal max-w-xs">
-              Variable fee tied to qualified lead generated at the booth.
+              Variable fee based on qualified leads and deals closed
             </p>
           </div>
         </div>
