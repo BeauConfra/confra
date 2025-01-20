4ee8f5ec9000725e601bb18057b80f2926005d52
BeauConfra <beau@confra.xyz>
2025-01-20 14:23:15 +1100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,19 @@
-// src/components/Header.js
+'use client'
 
 import React from 'react'
+import { usePathname } from 'next/navigation'
 
 export default function Footer() {
+  const pathname = usePathname()
+  const isHome = pathname === '/'
+
   return (
-    <footer className="pb-4 text-center text-white/70 text-sm">
-        © 2024 Confra. All Rights Reserved.
+    <footer
+      className={`pb-6 text-center text-sm ${
+        !isHome ? 'bg-white text-gray-900' : 'text-white/70'
+      }`}
+    >
+      © 2024 Confra. All Rights Reserved.
     </footer>
   )
 }
-
